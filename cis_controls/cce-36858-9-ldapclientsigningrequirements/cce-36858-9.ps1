65edf7e344dc4d82530fb7f7556f76b1769a785c
Kris Turner <13710532+kristopherjturner@users.noreply.github.com>
2019-09-28 17:36:59 -0500

    add new policies

---
+++ cis_controls/cce-36858-9-ldapclientsigningrequirements/cce-36858-9.ps1
@@ -0,0 +1,14 @@
+﻿Configuration LDAPClientSigningRequirements {
+
+	Import-DSCResource -ModuleName 'SecurityPolicyDSC' -ModuleVersion 2.8.0.0
+
+Node localhost{
+    SecurityOption AccountSecurityOptions {
+        Name = 'AccountSecurityOptions'
+        Network_security_LDAP_client_signing_requirements = 'Negotiate signing'
+    }
+    }
+}   
+
+# Compile the configuration to create the MOF files
+LDAPClientSigningRequirements -OutputPath 'C:\BaselineManagement\'
